8beb77a4187d606d2ab05829ad139ab7e8a87109
Vikas Poute <106815053+VikasPoute@users.noreply.github.com>
2023-05-23 03:50:09 +0530

    JPA next topics

--- jpa/src/main/java/com/spring/boot/jpa/dao/UserRepository.java
+++ jpa/src/main/java/com/spring/boot/jpa/dao/UserRepository.java
@@ -1,9 +1,27 @@
 package com.spring.boot.jpa.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.spring.boot.jpa.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+    public List<User> findByName(String name);
+
+    public List<User> findByCity(String city);
+
+    // @Query("SELECT u FROM User u")
+    // public List<User> getAllUsers();
+
+    @Query("SELECT u FROM User u WHERE u.name = :name")
+    public List<User> getUserByName(@Param("name") String name);
+
+    public List<User> getUserByNameAndCity(String name, String city);
+
+    @Query(value = "SELECT * FROM user", nativeQuery = true)
+    public List<User> getAllUsers();
 }
